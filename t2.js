bdd75656478a3a68baced14d973bf06563b3d69d
vmelekhova <vmelekhova@stud.khpi.ucode-connect.study>
2022-08-12 21:29:34 +0300

    added: task2

---
+++ t2.js
@@ -0,0 +1,8 @@
+const searchWordBySub = (string) => {
+    var regex = RegExp("хром");
+    return string.split(" ").find((elem, index) => {
+        return regex.test(elem);
+    })
+}
+
+// console.log(searchWordBySub('Ми знаємо, що монохромний колір – це градації сірого'))
