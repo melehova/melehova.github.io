0caeecc42c95bb47950d63e39c03c75c776dc930
vmelekhova <vmelekhova@stud.khpi.ucode-connect.study>
2022-08-13 13:46:53 +0300

    added: task5

---
+++ t5.js
@@ -0,0 +1,11 @@
+const getNames = (string) => {
+    var regex = RegExp(/\w+, \w+/g);
+    let result = ''
+    string.match(regex).forEach(name => {
+        result += `${name.split(', ')[1]} ${name.split(', ')[0]}\n` // replace
+    });
+    return result.slice(0, -1) // remove extra \n
+}
+
+let names = 'Lennon, John\nMcCartney, Paul\nHarrison, George\nStar, Ringo'
+console.log(getNames(names))
